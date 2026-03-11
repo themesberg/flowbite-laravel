df3103926475182042ac55a283ffb0010e588474
Dominique Thomas <dthomas@codenco.fr>
2026-03-11 08:32:48 +0100

    fix: docs

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,8 +3,8 @@
 ])
 
 <a {{ $attributes }} class="flex items-center gap-2 font-medium">
-{{--    <span class="flex aspect-square size-8 items-center justify-center rounded-md bg-gray-900 dark:bg-white">--}}
-{{--        <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />--}}
-{{--    </span>--}}
+    <span class="flex aspect-square size-8 items-center justify-center rounded-md bg-gray-900 dark:bg-white">
+        <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+    </span>
     <span class="text-sm font-semibold text-gray-900 dark:text-white">{{ config('app.name', 'Laravel') }}</span>
 </a>
